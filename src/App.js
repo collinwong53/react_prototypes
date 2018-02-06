cf9ebb229376e5a99acc53ee7a3410d77d26a8b9
collin_wong <collinwong53@gmail.com>
2018-02-05 21:59:39 -0800

    finished header

--- src/App.js
+++ src/App.js
@@ -8,11 +8,28 @@ class App extends Component {
     super(props);
     this.data = props.data;
   }
+  random(array){
+    console.log(array.length);
+    
+    let random = Math.floor(Math.random()*array.length);
+    return random;
+  }
   render() {
     console.log('data: ',data);
+    let random = this.random(data.user.favoriteQuotes);
+    console.log(random);
     return (
       <div className="App">
-        <h1>Super Blog</h1>
+        <div className="topHeader">
+          <h1>Super Blog</h1>
+          <div className="userProfile">
+            <div className="name">{data.user.name}</div>
+            <div className="quote">{data.user.favoriteQuotes[random]}</div>
+            <div className="avatar" style={{
+                backgroundImage:`url(${data.user.avatar})`,
+            }}></div>
+          </div>
+        </div>
       </div>
     );
   }
